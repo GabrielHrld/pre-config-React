5c88303a627139540b7aadb8a4c3109d8dc1750e
Gabi <g.federico.rodriguez@gmail.com>
2021-01-04 22:07:56 -0300

    file-loader ready

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,14 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   plugins: [
